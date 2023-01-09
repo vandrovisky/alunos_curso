0e530aa62d3405c154760a0f526277c04ad5180f
vandrovsky <ericrrp37@gmail.com>
2023-01-08 21:00:59 -0300

    apt3

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">home </div>
+
+                <div class="card-body">
+                    home carai 
+                    
+                </div>
+                // TODO: implementar caminho para alunos cursos e aluno-cursos
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,67 +1,95 @@
 <template>
+    
     <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Login Vue </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Login (Componente Vue)</div>
+                    <div class="card-body">
+                        <form method="POST" action="" @submit.prevent="login($event)">
+                            <input type="hidden" name="_token" :value="csrf_token">
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
 
-                <div class="card-body">
-                    <form method="POST" action="">
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus>
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control " name="password" required autocomplete="current-password">
-
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" >
-
-                                    <label class="form-check-label" for="remember">
-                                       mantenha conectado
-                                    </label>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus v-model="email">
                                 </div>
                             </div>
-                        </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    login
-                                </button>
 
-                                <a class="btn btn-link" href="#">
-                                    esqui a senha deste carai 
-                                </a>
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">Senha</label>
 
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password" v-model="password">
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember">
+
+                                        <label class="form-check-label" for="remember">
+                                            Mantenha-me conectado
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Login
+                                    </button>
+
+                                
+                                    <a class="btn btn-link" href="#">
+                                        Esqueci a senha
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    </div>
+
 </template>
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+        props: ['csrf_token'],
+        data() {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            login(e){
+
+                let url = 'http://127.0.0.1:8000/api/login';
+                let config = {
+                    method: 'post',
+                    body: new URLSearchParams({
+                        'email': this.email,
+                        'password': this.password
+
+                    })
+                }
+
+                fetch(url, config)
+                    .then(response => response.json())
+                    .then(data => {
+                        if(data.token){
+                            document.cookie = 'token='+data.token
+                        }
+                        e.target.submit()
+                    })
+               
+            }       
+        } 
     }
 </script>
 
